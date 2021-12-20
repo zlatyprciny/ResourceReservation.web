0fb594ff392424d5697ed616c585d8d915063c8c
zlatyprciny <zlaty.prciny@seznam.cz>
2021-12-20 19:40:27 +0100

    getOffices selector

--- src/app/days-office/state/index.ts
+++ src/app/days-office/state/index.ts
@@ -1,18 +1,23 @@
+import { state } from '@angular/animations';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import * as AppState from "../../state/app.state";
-import { daysOfficeFeatureKey, DaysOfficeState } from "./daysoffice.reducer";
-
-
+import { Office } from 'src/app/shared/models/ambulatory/office';
+import * as AppState from '../../state/app.state';
+import { daysOfficeFeatureKey, DaysOfficeState } from './daysoffice.reducer';
 
 export interface State extends AppState.State {
-   daysOffices: DaysOfficeState
+  daysOffices: DaysOfficeState;
 }
 
-const getProductFeatureState = createFeatureSelector<DaysOfficeState>(daysOfficeFeatureKey);
+const getProductFeatureState =
+  createFeatureSelector<DaysOfficeState>(daysOfficeFeatureKey);
 
 export const getDaysOffice = createSelector(
   getProductFeatureState,
-  state => state.daysOffices
+  (state) => state.daysOffices
 );
 
-
+export const getOffices = createSelector(getDaysOffice, (daysOffices) => {
+  let office: Office[] = [];
+  daysOffices.map((day) => office.push(day.office));
+  return office;
+});
