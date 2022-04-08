2a3e05852837cb2578496327d86ce6e08a15644b
zlatyprciny <zlaty.prciny@seznam.cz>
2022-04-08 09:08:08 +0200

    day summary menu implementation

--- src/app/days-office/day-summary/day-summary.component.ts
+++ src/app/days-office/day-summary/day-summary.component.ts
@@ -4,37 +4,21 @@ import { DayOffice } from '../day-office';
 @Component({
   selector: 'dm-day-summary',
   templateUrl: './day-summary.component.html',
-  styleUrls: ['./day-summary.component.scss']
+  styleUrls: ['./day-summary.component.scss'],
 })
 export class DaySummaryComponent implements OnInit {
   @Input() dayOffice: DayOffice;
   dataSource: any;
-  isContextMenuVisible: boolean;
   constructor() {
-    this.dataSource = [{
-      text: 'Share',
-      icon: 'dx-icon-globe',
-      items: [
-        { text: 'Facebook' },
-        { text: 'Twitter' }],
-    },
-    { text: 'Download', icon: 'dx-icon-download' },
-    { text: 'Add Comment', icon: 'dx-icon-add' },
-    { text: 'Add to Favorite', icon: 'dx-icon-favorites' },
+    this.dataSource = [
+      { text: 'Update', icon: 'dx-icon-edit' },
+      { text: 'Delete', icon: 'dx-icon-remove' },
     ];
-    this.isContextMenuVisible = false;
   }
   itemClick(e: any) {
     if (!e.itemData.items) {
       console.log(`The "${e.itemData.text}" item was clicked`, 'success', 1500);
     }
-    this.isContextMenuVisible = false;
   }
-  menuClick() {
-    this.isContextMenuVisible = true;
-  }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
